8f3f23a7708396a614f5acd5203b6f39d25728ec
Benjamin Bourgouin <67418255+KayleoRakeon@users.noreply.github.com>
2021-04-08 15:38:36 +0200

    exercice 3 terminé

---
+++ 03-DrumKit/script.js
@@ -0,0 +1,27 @@
+var keys = document.querySelectorAll(".key");
+var audio = document.querySelectorAll("audio");
+var thisKey;
+
+document.addEventListener('keydown', function (e) {
+    for (i = 0; i < keys.length; i++) {
+        if (e.keyCode == keys[i].dataset.key) {
+            keys[i].classList.add("playing");
+
+
+            for (j = 0; j < audio.length; j++) {
+                if (e.keyCode == audio[j].dataset.key) {
+                    audio[j].load();
+                    audio[j].play();
+                }
+            }
+        }
+    }
+});
+
+document.addEventListener("keyup", function (e) {
+    for (i = 0; i < keys.length; i++) {
+        if (e.keyCode == keys[i].dataset.key) {
+            keys[i].classList.remove("playing");
+        }
+    }
+});
